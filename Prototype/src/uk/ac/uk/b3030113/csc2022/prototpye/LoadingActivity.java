50fa14cd23798d329f8688d3a5513c500cb1c05e
Dennis Zinzi <d.zinzi@ncl.ac.uk>
2015-02-02 21:36:47 +0000

    Implemented Loading Screen

---
+++ Prototype/src/uk/ac/uk/b3030113/csc2022/prototpye/LoadingActivity.java
@@ -0,0 +1,58 @@
+package uk.ac.uk.b3030113.csc2022.prototpye;
+
+import java.util.Timer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LoadingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_loading);
+		
+		final Context context = this;
+		new Handler().postDelayed(new Runnable() {
+			 
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+ 
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(context, LoginActivity.class);
+                startActivity(i);
+ 
+                // close this activity
+                finish();
+            }
+        }, 3000);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.loading, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
