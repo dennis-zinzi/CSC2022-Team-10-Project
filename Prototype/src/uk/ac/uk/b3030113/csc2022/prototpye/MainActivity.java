7dc0ff92f8b21702104d09308d1608e9fa742e75
Dennis Zinzi <d.zinzi@ncl.ac.uk>
2015-02-02 19:40:01 +0000

    Added Transfer to Nav Bar

--- Prototype/src/uk/ac/uk/b3030113/csc2022/prototpye/MainActivity.java
+++ Prototype/src/uk/ac/uk/b3030113/csc2022/prototpye/MainActivity.java
@@ -53,14 +53,15 @@ NavigationDrawerFragment.NavigationDrawerCallbacks{
 		// update the main content by replacing fragments
 		FragmentManager fragmentManager = getFragmentManager();
 		switch (position+1) {
-		case 1:
-
-			fragmentManager.beginTransaction().replace(R.id.container, PlaceholderFragment.newInstance(position+1)).commit();
-			break;
-			
-		case 2:
-			fragmentManager.beginTransaction().replace(R.id.container, BalanceFragment.newInstance(position+1)).commit();
-			break;
+			case 1:
+				fragmentManager.beginTransaction().replace(R.id.container, PlaceholderFragment.newInstance(position+1)).commit();
+				break;
+			case 2:
+				fragmentManager.beginTransaction().replace(R.id.container, BalanceFragment.newInstance(position+1)).commit();
+				break;
+			case 3:
+				fragmentManager.beginTransaction().replace(R.id.container, TransferFragment.newInstance(position+1)).commit();
+				break;
 		}
 	}
 
@@ -68,37 +69,37 @@ NavigationDrawerFragment.NavigationDrawerCallbacks{
 	public void onSectionAttached(int number) {
 		final Context context = this;
 		switch (number) {
-		case 1:
-			break;
-		case 2:
-//			
-//			Intent balance = new Intent(context, BalanceActivity.class);
-//			startActivity(balance);
-//			break;
-		case 3:
-//			
-//			Intent transfer = new Intent(context, TransferActivity.class);
-//			startActivity(transfer);
-			break;
-		case 4:
-			
-//			Intent wallet = new Intent(context,WalletActivity.class);
-//			startActivity(wallet);
-			break;
-		case 5:
-//			
-//			Intent banking = new Intent(context, BankingActivity.class);
-//			startActivity(banking);
-			break;
-		case 6:
-//			
-//			Intent help = new Intent(context,HelpActivity.class);
-//			startActivity(help);
-			break;
-		case 7:
-//			Intent logout = new Intent(context,LoginActivity.class);
-//			startActivity(logout);
-			break;
+			case 1:
+				break;
+			case 2:
+			//			
+			//			Intent balance = new Intent(context, BalanceActivity.class);
+			//			startActivity(balance);
+			//			break;
+			case 3:
+			//			
+			//			Intent transfer = new Intent(context, TransferActivity.class);
+			//			startActivity(transfer);
+				break;
+			case 4:
+
+			//			Intent wallet = new Intent(context,WalletActivity.class);
+			//			startActivity(wallet);
+				break;
+			case 5:
+			//			
+			//			Intent banking = new Intent(context, BankingActivity.class);
+			//			startActivity(banking);
+				break;
+			case 6:
+			//			
+			//			Intent help = new Intent(context,HelpActivity.class);
+			//			startActivity(help);
+				break;
+			case 7:
+			//			Intent logout = new Intent(context,LoginActivity.class);
+			//			startActivity(logout);
+				break;
 
 
 		}
@@ -170,25 +171,59 @@ NavigationDrawerFragment.NavigationDrawerCallbacks{
 					.getInt(ARG_SECTION_NUMBER));
 		}
 	}
+	
+	
+	
 
 	public static class BalanceFragment extends Fragment {
+		private static final String ARG_SECTION_NUMBER = "section_number";
+
+		public static BalanceFragment newInstance(int sectionNumber) {
+			BalanceFragment fragment = new BalanceFragment();
+			Bundle args = new Bundle();
+			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+			fragment.setArguments(args);
+			return fragment;
+		}
+		public BalanceFragment() {
+
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_balance, container,
+					false);
+
+			return rootView;
+		}
+		@Override
+		public void onAttach(Activity activity) {
+			super.onAttach(activity);
+			((MainActivity) activity).onSectionAttached(getArguments().getInt(
+					ARG_SECTION_NUMBER));
+		}
+	}
+	
+	
+	public static class TransferFragment extends Fragment {
 	    private static final String ARG_SECTION_NUMBER = "section_number";
 	 
-	    public static BalanceFragment newInstance(int sectionNumber) {
-	        BalanceFragment fragment = new BalanceFragment();
+	    public static TransferFragment newInstance(int sectionNumber) {
+	        TransferFragment fragment = new TransferFragment();
 	        Bundle args = new Bundle();
 	        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
 	        fragment.setArguments(args);
 	        return fragment;
 	    }
-	    public BalanceFragment() {
+	    public TransferFragment() {
 	 
 	    }
 	 
 	    @Override
 	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	            Bundle savedInstanceState) {
-	        View rootView = inflater.inflate(R.layout.fragment_balance, container,
+	        View rootView = inflater.inflate(R.layout.fragment_transfer, container,
 	                false);
 	 
 	        return rootView;
