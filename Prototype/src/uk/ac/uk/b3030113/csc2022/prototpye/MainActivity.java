f1ec0643c2d45761a73bcd99f6dcaad6a0f04af2
Dennis Zinzi <d.zinzi@ncl.ac.uk>
2015-02-02 20:59:43 +0000

    Made Navigation Fully functional accross pages, made main menu buttons functional, and adopted Fragment appraoch instead of different activities for ease of implementation of Nav Bar and moving accross pages.

--- Prototype/src/uk/ac/uk/b3030113/csc2022/prototpye/MainActivity.java
+++ Prototype/src/uk/ac/uk/b3030113/csc2022/prototpye/MainActivity.java
@@ -11,14 +11,16 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.support.v4.widget.DrawerLayout;
 import android.content.Intent;
 //import android.graphics.Color;
 //import android.net.Uri;
 
 public  class MainActivity extends Activity implements
-NavigationDrawerFragment.NavigationDrawerCallbacks{
+NavigationDrawerFragment.NavigationDrawerCallbacks, OnClickListener{
 
 	/**
 	 * Fragment managing the behaviors, interactions and presentation of the
@@ -30,13 +32,13 @@ NavigationDrawerFragment.NavigationDrawerCallbacks{
 	 * Used to store the last screen title. For use in
 	 * {@link #restoreActionBar()}.
 	 */
-
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//addListenerOnButton();
 
 		mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager()
 				.findFragmentById(R.id.navigation_drawer);
@@ -46,6 +48,9 @@ NavigationDrawerFragment.NavigationDrawerCallbacks{
 		mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
 				(DrawerLayout) findViewById(R.id.drawer_layout));
 	}
+
+
+
 
 
 	@Override
@@ -62,48 +67,27 @@ NavigationDrawerFragment.NavigationDrawerCallbacks{
 			case 3:
 				fragmentManager.beginTransaction().replace(R.id.container, TransferFragment.newInstance(position+1)).commit();
 				break;
+			case 4:
+				fragmentManager.beginTransaction().replace(R.id.container, WalletFragment.newInstance(position+1)).commit();
+				break;
+			case 5:
+				fragmentManager.beginTransaction().replace(R.id.container, BankingFragment.newInstance(position+1)).commit();
+				break;
+			case 6:
+				fragmentManager.beginTransaction().replace(R.id.container, HelpFragment.newInstance(position+1)).commit();
+				break;
+			case 7:
+				Intent logout = new Intent(this,LoginActivity.class);
+				startActivity(logout);
+				break;
 		}
 	}
 
 
 	public void onSectionAttached(int number) {
-		final Context context = this;
-		switch (number) {
-			case 1:
-				break;
-			case 2:
-			//			
-			//			Intent balance = new Intent(context, BalanceActivity.class);
-			//			startActivity(balance);
-			//			break;
-			case 3:
-			//			
-			//			Intent transfer = new Intent(context, TransferActivity.class);
-			//			startActivity(transfer);
-				break;
-			case 4:
 
-			//			Intent wallet = new Intent(context,WalletActivity.class);
-			//			startActivity(wallet);
-				break;
-			case 5:
-			//			
-			//			Intent banking = new Intent(context, BankingActivity.class);
-			//			startActivity(banking);
-				break;
-			case 6:
-			//			
-			//			Intent help = new Intent(context,HelpActivity.class);
-			//			startActivity(help);
-				break;
-			case 7:
-			//			Intent logout = new Intent(context,LoginActivity.class);
-			//			startActivity(logout);
-				break;
-
-
-		}
 	}
+
 
 
 
@@ -131,6 +115,23 @@ NavigationDrawerFragment.NavigationDrawerCallbacks{
 		return super.onOptionsItemSelected(item);
 	}
 
+	
+	
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	/**
 	 * A placeholder fragment containing a simple view.
@@ -161,7 +162,79 @@ NavigationDrawerFragment.NavigationDrawerCallbacks{
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main,
 					container, false);
+			
+			
+			Button qBalance;
+			Button fBanking;
+			Button help;
+			Button logout;
+			Button wallets;
+			Button qTransfer;
+
+
+			
+			//Determine button to add listener to
+			qBalance = (Button) rootView.findViewById(R.id.qBalance);
+			fBanking = (Button) rootView.findViewById(R.id.fBanking);
+			help = (Button) rootView.findViewById(R.id.help);
+			logout = (Button) rootView.findViewById(R.id.logout);
+			wallets = (Button) rootView.findViewById(R.id.wallets);
+			qTransfer = (Button) rootView.findViewById(R.id.qTransfer);
+
+			final FragmentManager fragmentManager = getFragmentManager();
+			
+			
+			
+			qBalance.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v){
+					fragmentManager.beginTransaction().replace(R.id.container, BalanceFragment.newInstance(2)).commit();
+				}
+
+			});
+			qTransfer.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v){
+					fragmentManager.beginTransaction().replace(R.id.container, TransferFragment.newInstance(3)).commit();
+				}
+
+			});
+			wallets.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v){
+					fragmentManager.beginTransaction().replace(R.id.container, WalletFragment.newInstance(4)).commit();
+				}
+
+			});
+			fBanking.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v){
+					fragmentManager.beginTransaction().replace(R.id.container, BankingFragment.newInstance(5)).commit();
+				}
+
+			});
+			help.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v){
+					fragmentManager.beginTransaction().replace(R.id.container, HelpFragment.newInstance(6)).commit();
+				}
+
+			});
+			logout.setOnClickListener(new OnClickListener(){
+				
+				@Override
+				public void onClick(View v){
+					Intent logout = new Intent(getActivity(), LoginActivity.class);
+					startActivity(logout);
+				}
+
+			});
+			
+			
+			
+			
 			return rootView;
+			
 		}
 
 		@Override
@@ -171,9 +244,9 @@ NavigationDrawerFragment.NavigationDrawerCallbacks{
 					.getInt(ARG_SECTION_NUMBER));
 		}
 	}
-	
-	
-	
+
+
+
 
 	public static class BalanceFragment extends Fragment {
 		private static final String ARG_SECTION_NUMBER = "section_number";
@@ -204,26 +277,88 @@ NavigationDrawerFragment.NavigationDrawerCallbacks{
 					ARG_SECTION_NUMBER));
 		}
 	}
-	
-	
+
+
 	public static class TransferFragment extends Fragment {
+		private static final String ARG_SECTION_NUMBER = "section_number";
+
+		public static TransferFragment newInstance(int sectionNumber) {
+			TransferFragment fragment = new TransferFragment();
+			Bundle args = new Bundle();
+			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+			fragment.setArguments(args);
+			return fragment;
+		}
+		public TransferFragment() {
+
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_transfer, container,
+					false);
+
+			return rootView;
+		}
+		@Override
+		public void onAttach(Activity activity) {
+			super.onAttach(activity);
+			((MainActivity) activity).onSectionAttached(getArguments().getInt(
+					ARG_SECTION_NUMBER));
+		}
+	}
+
+	public static class WalletFragment extends Fragment {
+		private static final String ARG_SECTION_NUMBER = "section_number";
+
+		public static WalletFragment newInstance(int sectionNumber) {
+			WalletFragment fragment = new WalletFragment();
+			Bundle args = new Bundle();
+			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+			fragment.setArguments(args);
+			return fragment;
+		}
+		public WalletFragment() {
+
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_wallet, container,
+					false);
+
+			return rootView;
+		}
+		@Override
+		public void onAttach(Activity activity) {
+			super.onAttach(activity);
+			((MainActivity) activity).onSectionAttached(getArguments().getInt(
+					ARG_SECTION_NUMBER));
+		}
+	}
+	
+	
+	
+	public static class BankingFragment extends Fragment {
 	    private static final String ARG_SECTION_NUMBER = "section_number";
 	 
-	    public static TransferFragment newInstance(int sectionNumber) {
-	        TransferFragment fragment = new TransferFragment();
+	    public static BankingFragment newInstance(int sectionNumber) {
+	        BankingFragment fragment = new BankingFragment();
 	        Bundle args = new Bundle();
 	        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
 	        fragment.setArguments(args);
 	        return fragment;
 	    }
-	    public TransferFragment() {
+	    public BankingFragment() {
 	 
 	    }
 	 
 	    @Override
 	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	            Bundle savedInstanceState) {
-	        View rootView = inflater.inflate(R.layout.fragment_transfer, container,
+	        View rootView = inflater.inflate(R.layout.fragment_banking, container,
 	                false);
 	 
 	        return rootView;
@@ -235,4 +370,42 @@ NavigationDrawerFragment.NavigationDrawerCallbacks{
 	                ARG_SECTION_NUMBER));
 	    }
 	}
+	
+	
+
+
+	public static class HelpFragment extends Fragment {
+	    private static final String ARG_SECTION_NUMBER = "section_number";
+	 
+	    public static HelpFragment newInstance(int sectionNumber) {
+	        HelpFragment fragment = new HelpFragment();
+	        Bundle args = new Bundle();
+	        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+	        fragment.setArguments(args);
+	        return fragment;
+	    }
+	    public HelpFragment() {
+	 
+	    }
+	 
+	    @Override
+	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	            Bundle savedInstanceState) {
+	        View rootView = inflater.inflate(R.layout.fragment_help, container,
+	                false);
+	 
+	        return rootView;
+	    }
+	    @Override
+	    public void onAttach(Activity activity) {
+	        super.onAttach(activity);
+	        ((MainActivity) activity).onSectionAttached(getArguments().getInt(
+	                ARG_SECTION_NUMBER));
+	    }
+	}
+
+
+
+
 }
+
